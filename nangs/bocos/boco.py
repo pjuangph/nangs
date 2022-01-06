279c257390da9b9ce440c0a3c563632ae5d3bd2c
Paht J <paht.juangphanich@gmail.com>
2022-01-05 23:00:14 -0500

    more docs

--- nangs/bocos/boco.py
+++ nangs/bocos/boco.py
@@ -1,8 +1,12 @@
-import torch
+from abc import ABC # abstract class python
 
+class Boco(ABC):
+    def __init__(self, name:str):
+        """Base class for setting up boundary conditions
 
-class Boco():
-    def __init__(self, name):
+        Args:
+            name (str): name of the boundary conditions. Example: "Initial conditions"
+        """
         self.name = name
 
     def validate(self):
--- nangs/bocos/dirichlet.py
+++ nangs/bocos/dirichlet.py
@@ -1,10 +1,21 @@
+from typing import Tuple
 import numpy as np
 import torch
 from .boco import Boco
-
+from ..samplers import base_sampler
 
 class Dirichlet(Boco):
-    def __init__(self, sampler, output_fn, name="dirichlet"):
+    def __init__(self, sampler:base_sampler, output_fn:function, name:str="dirichlet"):
+        """The Dirichlet boundary condition is a type 1 boundary condition which means it is used to specify constant function values at given points. There are no derivatives being specified.
+
+        More Info:
+            https://www.simscale.com/docs/simwiki/numerics-background/what-are-boundary-conditions/
+
+        Args:
+            sampler (base_sampler): [description]
+            output_fn (function): [description]
+            name (str, optional): [description]. Defaults to "dirichlet".
+        """
         super().__init__(name)
         self.vars = sampler.vars
         self.sampler = sampler
@@ -27,7 +38,22 @@ class Dirichlet(Boco):
         outputs = self.output_fn(inputs)
         return inputs, outputs
 
-    def computeLoss(self, model, criterion, inputs, outputs):
+    def computeLoss(self, model:torch.nn.Module, criterion, inputs:Tuple[str], outputs:Tuple[str]):
+        """This function computes the loss from the model. The model is expected to predict values for all states of the PDE so if the PDE takes x,y,t and predicts phi. Then the model will predict the value of phi at x,y,t for all time.
+
+        Example:
+            This function takes the boundary conditions so if you specify the initial condition at u = something for t = 0. 
+            It checks how well the model is at predicting u
+
+        Args:
+            model (torch.nn.Module): torch model
+            criterion (torch.nn._Loss): loss criterion like MSE
+            inputs (Tuple[str]): Tuple containing keys for the input variables e.g. ('x','y','t')
+            outputs (Tuple[str]): Tuple containing keys for the output variables e.g. ('u','v')
+
+        Returns:
+            Dict[str,torch.Tensor]: Dictionary containing the name of the boundary condition and the loss value
+        """
         _X, _y = self.sample()
         X = torch.stack([
             _X[var]
